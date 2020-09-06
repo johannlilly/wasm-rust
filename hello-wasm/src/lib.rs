1d7c29c4a9456f7b1a28587facb3cdfe618e81fd
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-09-05 21:48:05 -0400

    wasm-pack uses wasm-bindgen, another tool, to provide a bridge between the types of JavaScript and Rust. It allows JavaScript to call a Rust API with a string, or a Rust function to catch a JavaScript exception.

--- hello-wasm/src/lib.rs
+++ hello-wasm/src/lib.rs
@@ -1,5 +1,6 @@
 // use wasm-bindgen to commmunicate between Rust and JavaScript
 // import code from a library into you code
+// wasm-pack uses wasm-bindgen to provide a bridge between the types of JavaScript and Rust. It allows JavaScript to call a Rust API with a string, or a Rust function to catch a JavaScript exception
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
