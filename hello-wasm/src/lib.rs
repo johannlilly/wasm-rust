1f0f3379303a721083940e6c1b1feadb1397e790
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-09-05 21:45:34 -0400

    Using wasm-bindgen to communicate between Rust and JavaScript

--- hello-wasm/src/lib.rs
+++ hello-wasm/src/lib.rs
@@ -1,7 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+// use wasm-bindgen to commmunicate between Rust and JavaScript
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
 }
